02cd2f4f207f88565b201b46b527ab7c59e1d6c7
Josef Bnm <ssefbnm@gmail.com>
2020-07-12 16:59:25 +0100

    add home product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { NavComponent } from './home/nav/nav.component';
 import { HeaderComponent } from './home/header/header.component';
+import { CategoriesComponent } from './home/categories/categories.component';
+import { HomeProductsComponent } from './shared/home-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent,
     HeaderComponent,
+    CategoriesComponent,
+    HomeProductsComponent,
   ],
   imports: [
     BrowserModule,
